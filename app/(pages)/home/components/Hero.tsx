6279f98516dd86c710c7a28fe76b1a400da0a052
Mahmoud Hassan <mahmoud.hassan@cyshield.com>
2024-10-20 13:33:44 +0300

    home hero section

---
+++ app/(pages)/home/components/Hero.tsx
@@ -0,0 +1,30 @@
+import { Container } from "@/components/shared/container";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+type Props = {};
+
+export const Hero = (props: Props) => {
+	return (
+		<Container className="grid grid-cols-1 items-center gap-[85px] lg:grid-cols-2 py-[50px] lg:pt-[30px] lg:pb-[70px]">
+			<div>
+				<h1 className="text-[56px] font-[700] text-center lg:text-start leading-[1]">
+					Discover Your Dream Home Today
+				</h1>
+				<p className="mt-[40px] leading-[22px] text-center lg:text-start">
+					Find the perfect property that suits your lifestyle. Our real estate
+					service offers unique selling propositions that set us apart from the
+					competition. Find your perfect home today!
+				</p>
+
+				<div className="mt-[64px] flex items-center gap-[16px] justify-center lg:justify-start">
+					<Button className="font-[600]">Explore Now</Button>
+					<Button variant="black" className="bg-transparent text-black hover:text-white border border-black font-[600] lg:font-normal">
+						Learn more
+					</Button>
+				</div>
+			</div>
+			<Image src="/images/hero.webp" width={1000} height={1000}  className="w-auto rounded-[16px]" alt="hero image"/>
+		</Container>
+	);
+};
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/app/(pages)/home/components/Hero";
+
+export default function Home() {
+	return (
+		<div className="font-[family-name:var(--font-geist-sans)]">
+			<Hero />
+		</div>
+	);
+}
