8845ad592ebce7e7651f4bcd3333523f88ac0685
Mahmoud Hassan <mahmoud.hassan@cyshield.com>
2024-10-20 11:26:54 +0300

    Footer copyrights

---
+++ components/atoms/Copyrights.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type Props = {}
+
+export const Copyrights = (props: Props) => {
+  return <div className="text-center text-[18px] mt-[18px] hidden md:block">Copyright © AyEstate | Designed by AY Studio</div>;
+}
--- components/atoms/footer.tsx
+++ components/atoms/footer.tsx
@@ -1,11 +1,15 @@
 import { Container } from "../shared/container";
+import { Copyrights } from "./Copyrights";
 
 type Props = {};
 
 export const Footer = (props: Props) => {
-  return (
-    <footer>
-      <Container>Footer</Container>
-    </footer>
-  );
+	return (
+		<footer className="bg-black-2 py-[50px] text-white lg:pb-[32px] lg:pt-[100px]">
+			<Container>
+				Footer
+				<Copyrights />
+			</Container>
+		</footer>
+	);
 };
