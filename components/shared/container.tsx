b6da84326384df3f4aeafed35eccfa4c63005ed0
Mahmoud Hassan <mahmoud.hassan@cyshield.com>
2024-10-20 14:49:52 +0300

    Container fix

--- components/shared/container.tsx
+++ components/shared/container.tsx
@@ -6,7 +6,7 @@ export const Container = ({ children, ...props }: Props) => {
   return (
     <div
       {...props}
-      className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${props.className}`}
+      className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-[20px] ${props.className}`}
     >
       {children}
     </div>
